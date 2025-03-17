3c9fa28e5fcd27f97ed5417cee69510e844a265c
Bilous Oleg <olezhka82802@gmail.com>
2025-03-17 14:17:28 +0200

    The game was reworked using OOP, added new mobs, each mob have its own stats

--- script.js
+++ script.js
@@ -1,66 +1,127 @@
-let start = document.querySelector(".start");
-let startButton = document.querySelector("#start-button");
-let gameField = document.querySelector("#game-area");
-let gameTimer = document.querySelector("#game-timer");
-let gamePoints = document.querySelector("#game-points");
+const start = document.querySelector(".start");
+const startButton = document.querySelector("#start-button");
+const gameField = document.querySelector("#game-area");
+const gameTimer = document.querySelector("#game-timer");
+const gamePoints = document.querySelector("#game-points");
 
-let monsterArray = [];
+const monsterArray = [];
 
-let options = {
-    monsterDelay: 1,    //Seconds
-    timerCounter: 30,    //Seconds
+let points = 0;
+
+class Monster{
+    constructor(points, jumpDelay, image, size){
+        this.points = points;
+        this.jumpDelay = jumpDelay;
+        this.image = image;
+        this.size = size;
+        this.element = this.generateDOM();
+        this.isDead = false;
+        this.jumpInter = setInterval(()=> {
+            this.element.style.left = `${Math.random() * (90.08-0) + 0}%`;
+            this.element.style.top = `${Math.random() * (82.55-0) + 0}%`;
+        }, this.jumpDelay * 1000);
+    }
+
+    generateDOM() {
+        let element = document.createElement("div");
+        element.classList.add("monster");
+        gameField.appendChild(element);
+        element.style.width = `${10 * this.size}%`;
+        element.style.height = `${17.7 * this.size}%`;
+        element.style.left = `${Math.random() * (90.08-0) + 0}%`;
+        element.style.top = `${Math.random() * (82.55-0) + 0}%`;
+        element.style.backgroundImage = `url("./assets/images/${this.image}.png")`
+        element.addEventListener("click", ()=> {
+            this.isDead = true;
+        })
+        return element;
+    }
+
+    deleteDOM() {
+        gameField.removeChild(this.element);
+        clearInterval(this.jumpInter);
+    }
+};
+
+const options = {
+    monsterDelay: 0.8,    //Seconds
+    timerCounter: 60,    //Seconds
     monsterCap: 5,
-    monsterJumpDelay: 2,    //Seconds
-    pointsToWin: 15
+    pointsToWin: 45,
+    monsterTypes: [
+        {
+            points: 2,
+            jumpDelay: 1,
+            image: "skeleton",
+            size: 1,
+            chance: 55
+        },
+        {
+            points: 4,
+            jumpDelay: 0.7,
+            image: "vex",
+            size: 0.5,
+            chance: 20
+        },
+        {
+            points: 1,
+            jumpDelay: 1.5,
+            image: "zombu",
+            size: 1,
+            chance: 75
+        }
+    ],
 }
 
 function func1() {
     document.body.removeChild(start);
-
-    let points = 0;
+    
     let spawnInter = setInterval(()=> {
         if (monsterArray.length == options.monsterCap) {
-            console.log(1);
+
         }
         else {
-            let monsterVar = document.createElement("div");
-            monsterArray.push(monsterVar);
-            gameField.appendChild(monsterVar);
-            monsterVar.classList.add("monster");
-            monsterVar.style.left = `${Math.random() * (90.08-0) + 0}%`;
-            monsterVar.style.top = `${Math.random() * (82.55-0) + 0}%`;
-
-            monsterVar.addEventListener ("click", ()=> {
-                points++;
-                gamePoints.textContent = `Очки: ${points}`;
-                gameField.removeChild(monsterVar);
-                monsterArray.splice(monsterArray.indexOf(monsterVar), 1);
-                if (points >= options.pointsToWin) {
-                    let end = document.createElement("div");
-                    let endMessage = document.createElement("div");
-                    end.classList.add("end-back");
-                    endMessage.classList.add("end-message");
-                    gameField.classList.add("gameEndArea");
-                    gameField.appendChild(end);
-                    gameField.appendChild(endMessage);
-                    endMessage.textContent = "Победа!";
-                    clearInterval(timerInter);
-                    clearInterval(spawnInter);
-                    clearInterval(jumpInter);
-                    clearTimeout(gameTimeout);
+            while (1) {
+                let type = options.monsterTypes[Math.floor(Math.random() * (options.monsterTypes.length - 0)) + 0];
+                if (Math.random() * 100 <= type.chance) {
+                    let monsterVar = new Monster(type.points, type.jumpDelay, type.image, type.size);
+                    monsterArray.push(monsterVar);
+                    break;
                 }
-            })
+            }
         }
     }, options.monsterDelay * 1000);
 
-    let jumpInter = setInterval (() => {
-        for (i = 0; i < options.monsterCap; i++) {
-            if (monsterArray.includes(monsterArray[i])){
-                monsterArray[i].style.left = `${Math.random() * (90.08-0) + 0}%`;
-                monsterArray[i].style.top = `${Math.random() * (82.55-0) + 0}%`;
+    let gameLoop = setInterval (() => {
+        let i = 0;
+        while (i < monsterArray.length) {
+            if (monsterArray[i].isDead == true) {
+                points += monsterArray[i].points;
+                gamePoints.textContent = `Очки: ${points}`;
+                monsterArray[i].deleteDOM();
+                monsterArray.splice(i, 1);
+                i--;
+            }
+            i++;
+        }
+        if (points >= options.pointsToWin) {
+            let end = document.createElement("div");
+            let endMessage = document.createElement("div");
+            end.classList.add("end-back");
+            endMessage.classList.add("end-message");
+            gameField.classList.add("gameEndArea");
+            gameField.appendChild(end);
+            gameField.appendChild(endMessage);
+            endMessage.textContent = "Победа!";
+            clearInterval(timerInter);
+            clearInterval(spawnInter);
+            clearInterval(gameLoop);
+            clearTimeout(gameTimeout);
+            for (let j = 0; j < monsterArray.length; j++) {
+                monsterArray[j].deleteDOM();
             }
         }
-    }, options.monsterJumpDelay * 1000);
+    }, 1);
     
     let timerInter = setInterval(() => {
         gameTimer.textContent = options.timerCounter - 1;
@@ -76,9 +137,12 @@ function func1() {
         gameField.appendChild(end);
         gameField.appendChild(endMessage);
         endMessage.textContent = "Поражение...";
+        for (let j = 0; j < monsterArray.length; j++) {
+            monsterArray[j].deleteDOM();
+        }
         clearInterval(timerInter);
         clearInterval(spawnInter);
-        clearInterval(jumpInter);
+        clearInterval(gameLoop);
     }, `${options.timerCounter * 1000}`);
 
 }
